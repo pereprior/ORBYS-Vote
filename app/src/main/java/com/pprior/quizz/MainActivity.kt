0c86343099840218d428f9274c62efd4a33a68ab
pere <pj.prior.es@gmail.com>
2024-03-20 14:13:58 +0100

    binding init

--- app/src/main/java/com/pprior/quizz/MainActivity.kt
+++ app/src/main/java/com/pprior/quizz/MainActivity.kt
@@ -1,14 +1,15 @@
 package com.pprior.quizz
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import com.pprior.quizz.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
